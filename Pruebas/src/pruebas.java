209d53b9f1005ecd36b180dc7569284ef2451a43
Mariluz <Mariluz@LAPTOP-T9E2GEV7.Home>
2019-03-28 20:16:14 +0100

    probando

---
+++ Pruebas/src/pruebas.java
@@ -0,0 +1,28 @@
+
+public class pruebas {
+
+    static int pepe;
+
+    private int atributo1; //creamos el atributo
+
+    pruebas (int pepe) {
+
+                  this.pepe= pepe;
+
+            }
+
+     public static int getAtrib1() {
+
+                    return pepe;
+
+       }
+
+    public static void main (String[ ] Args) {
+
+        System.out.println ("Veamos que hay en Pepe");
+
+        System.out.println ("Pepe es igual a " + pruebas.getAtrib1()); //eliminación del this.
+
+    } //Cierre del main
+
+}
