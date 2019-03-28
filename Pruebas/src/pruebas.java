25b124ed499c3b9523639b53bd26824cbaba5f7b
Mariluz <Mariluz@LAPTOP-T9E2GEV7.Home>
2019-03-28 22:40:03 +0100

    prueba 2

--- Pruebas/src/pruebas.java
+++ Pruebas/src/pruebas.java
@@ -19,7 +19,6 @@ public class pruebas {
 
     public static void main (String[ ] Args) {
 
-        System.out.println ("Veamos que hay en Pepe, cambiando");
 
         System.out.println ("Pepe es igual a " + pruebas.getAtrib1()); //eliminación del this.
 
